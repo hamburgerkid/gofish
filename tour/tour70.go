c6952ce3ab41a032030783ae99520e1e76434295
hamburgerkid <hamburgerkid@gmail.com>
2014-11-25 17:05:48 +0900

    Add a script for the tour 70.

---
+++ tour/tour70.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	// https://code.google.com/p/go-tour/source/browse/tree/tree.go
+	"code.google.com/p/go-tour/tree"
+	"fmt"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	Step(t, ch)
+	close(ch)
+}
+
+func Step(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Step(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Step(t.Right, ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	c1 := make(chan int)
+	c2 := make(chan int)
+	go Walk(t1, c1)
+	go Walk(t2, c2)
+	for i := range c1 {
+		if i != <-c2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	for i := range ch {
+		fmt.Println(i)
+	}
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
