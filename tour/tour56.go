5dcd8ba032c9d6cb77168df2f68d4ed597cbf163
mitsuharu.hamba <mitsuharu.hamba@mail.rakuten.com>
2014-11-25 12:57:27 +0900

    Add a script for the tour 56.

---
+++ tour/tour56.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %f", e)
+}
+
+func Sqrt(f float64) (float64, error) {
+	if f > 0 {
+		return math.Sqrt(f), nil
+	} else {
+		return 0, ErrNegativeSqrt(f)
+	}
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
